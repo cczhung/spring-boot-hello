0c8cfbbcc3b0e52054d702f9819a150577bc3bdf
cczhung <792996942@qq.com>
2018-12-04 17:01:39 +0800

    init

---
+++ src/main/java/com/neo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.neo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${env.test:'no env'}")
+    private String envSrting;
+
+    @RequestMapping("/")
+    public String index() {
+        return "test evn:"+envSrting;
+    }
+}
